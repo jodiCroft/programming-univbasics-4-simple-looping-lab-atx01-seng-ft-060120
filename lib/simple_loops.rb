69d0420fd35d6bbf198d9bfb104e509bf225ae0b
Jodi Croft <jodipruett@gmail.com>
2020-05-17 17:58:43 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,3 +24,13 @@ def output_array(array)
     count += 1
   end
 end
+
+def return_string_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count].to_s
+    new_array << array[count].to_s
+    count += 1
+  end
+  new_array
+end
